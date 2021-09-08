edaf0e68fc8ec1896873b5b1db09e561bdfe8ac4
Orangzaib Khan <Orangzaib Khan@DESKTOP-A94KPUN>
2021-09-08 16:31:43 +0500

    Profile page added.

---
+++ AMS/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace AMS.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Authorize(Roles = "Employee")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
